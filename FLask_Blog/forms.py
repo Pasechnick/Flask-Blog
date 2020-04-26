aee4608bd49a9143cacb29aa24a25ccd04047c1d
Alexander Mitupov <mitupov@gmail.com>
2020-04-26 16:24:54 +0700

    add a sqlalchemy database with user and post model, create a memo how to use sqlalchemy db through python3 shell

--- FLask_Blog/forms.py
+++ FLask_Blog/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo # with 
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
-                            validators=[DataRequired(), Length(min=2, max=20)]) # the length of the username must be from 2 to 20 characters
+                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                             validators=[DataRequired(), Email()])
     password = PasswordField('Password', 
