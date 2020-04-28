ae7717b94590cbab4d48852f2fc51786fd243336
Alexander Mitupov <mitupov@gmail.com>
2020-04-28 22:12:06 +0700

    reorganize the last commited files so it is the commit to look for, not the previous

--- FLask_Blog/flaskblog.py
+++ FLask_Blog/flaskblog.py
@@ -1,21 +1,15 @@
 
 from flask import Flask, render_template, url_for, flash, redirect
-
 from flask_sqlalchemy import SQLAlchemy 
 from forms import RegistrationForm, LoginForm
 
-from models import User, Post
 
 app = Flask(__name__)
-
-
-
 app.config['SECRET_KEY'] = '5e5b968d2e77c6d37d2b17ee2bc819de'
-
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
-
-
 db = SQLAlchemy(app) 
+
+from models import User, Post
  
 
 posts = [
