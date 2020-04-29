7448d567e74f774628ee959d31b50da7e34e1ac8
Alexander Mitupov <mitupov@gmail.com>
2020-04-29 23:59:08 +0700

    add logout, navigation bar logic for logged in status, account route, db user info check

--- FLask_Blog/flaskblog/models.py
+++ FLask_Blog/flaskblog/models.py
@@ -1,8 +1,19 @@
 from datetime import datetime
-from flaskblog import db
+from flaskblog import db, login_manager # imports that login_manager class from the init file
+from flask_login import UserMixin # class that provides us with attributes and methods for login  
+
+# we also need to create a function with a decoretor userloader - 
+# for reloading user from the user id stored in the session 
+# so we need a user id from the db
+
+# also the extention will expect to have the user model a surtain attributes and methods - 4 to be excact: "is authenticated", "is active", "is anonimus", "get id" 
+# of course we could add all 4 methods by ourself, but the extention privides us with the class UserMixin that we need to import   
+@login_manager.user_loader # we decorete this function  with the name convention this way so the extention knows that this is that "user id"
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin): # we need to add UserMixin ...
     id=db.Column(db.Integer, primary_key = True)
     
     username = db.Column(db.String(20), unique=True, nullable=False) 
--- FLask_Blog/flaskblog/routes.py
+++ FLask_Blog/flaskblog/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request # we need "request" to 
 from flaskblog import app, db, bcrypt # we need to import "db" and "bcrypt" to use that hash algorithm (remember that before we got the instance of bcrypt class with python3 shell)
 from flaskblog.forms import RegistrationForm, LoginForm 
 from flaskblog.models import User, Post
- 
+from flask_login import login_user, current_user, logout_user, login_required # imports that class so we can check the user email and password validation with the db data, and with current user we can deliver user a
+
 
 posts = [
     {
@@ -33,6 +34,8 @@ def about():
 
 @app.route("/register", methods =['GET', 'POST'])
 def register():
+    if current_user.is_authenticated: # current user check
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         #so if the form valid to submit (all length and signs are correct) we can hash the password and do the authentication procedure 
@@ -45,14 +48,34 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title = 'Register', form = form)
 
+
 @app.route("/login", methods =['GET', 'POST'])
 def login():
+    if current_user.is_authenticated: # current user check
+        return redirect(url_for('home'))
     form = LoginForm()
-    
     if form.validate_on_submit():
-        if form.email.data == "admin@blog.com" and form.password.data == "password":
-            flash("You have been logged in !", "success")
-            return redirect(url_for('home'))
+        # before we were simply checking the hardcoded data, and now we need to check the database for valid data 
+        # firstly the user will be logged in with an email, so we need to write a check for the email in the db:
+        user = User.query.filter_by(email = form.email.data).first() # finds the first User by entered email in the id
+        if user and bcrypt.check_password_hash(user.password, form.password.data): # so if the user exist and the password the user entered is valid we want to login the user and we also need to import the login user function 
+            login_user(user, remember = form.remember.data) # "user" - what we want to login, "remember" - remember me form (boolean) we have created
+            next_page = request.args.get('next') # this line
+            return redirect(next_page) if next_page else redirect(url_for('home')) # use of ternary conditional 
         else:
-            flash("login unsuccessful. Pls check ur username and password", "danger")
+            flash("login unsuccessful. Pls check ur email and password", "danger")
     return render_template('login.html', title = 'Login', form = form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required #extention knows that we need to be logged in to access this route 
+def account():
+    return render_template('account.html', title = 'Account')
+
+
