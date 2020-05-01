a7be2ff4a0f18900a89d402e0248f74e93dd0653
Alexander Mitupov <mitupov@gmail.com>
2020-05-01 18:38:33 +0700

    add update account, upload image as avatar, sizes all uploaded images to 125x125

--- FLask_Blog/flaskblog/forms.py
+++ FLask_Blog/flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed # need ths to upload files into the db, and allowance to be able to upload surtain files 
+from flask_login import current_user # we need current user for update user info (for data check )
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError  # also add ValidationError
-from flaskblog.models import User # we need to import User to make the validation Error message
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError 
+from flaskblog.models import User 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -14,12 +16,7 @@ class RegistrationForm(FlaskForm):
                                          validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    # with this template function we can prevent showing the flaskerror page instead with our error messages under the sign up fields (username, email...)
-    # ^it is taken from "wtf_forms" documentaton 
-    # def validate_field(self, field):
-    #     if True:
-    #         raise ValidationError('Validation Message')
-
+   
     def validate_username(self, username):
         user = User.query.filter_by(username = username.data).first()
         if user:
@@ -32,7 +29,6 @@ class RegistrationForm(FlaskForm):
 
 
 
-
 class LoginForm(FlaskForm):
     email = StringField('Email',
                             validators=[DataRequired(), Email()])
@@ -40,6 +36,32 @@ class LoginForm(FlaskForm):
                                 validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-    
+
+
+
+# the form for updating an account info in the account route
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])]) # with FileAllowed validator we can validate particular file extention                            
+    submit = SubmitField('Update')
+
+
+    # with current_user class we want to make a check, so if the username/email does not match to current user's we make an update 
+    def validate_username(self, username):
+        # the username validation check
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError('Validation Message: That Username is taken, please use another')
+
+    def validate_email(self, email):
+        # the email validation check
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('Validation Message: That Email is taken, please use another')
 
 
--- FLask_Blog/flaskblog/routes.py
+++ FLask_Blog/flaskblog/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template, url_for, flash, redirect, request # we need "request" to 
-from flaskblog import app, db, bcrypt # we need to import "db" and "bcrypt" to use that hash algorithm (remember that before we got the instance of bcrypt class with python3 shell)
-from flaskblog.forms import RegistrationForm, LoginForm 
+import os # need to grab the file extention of the uploaded image and saves it do db with this extention (image.jpg, image.png, ect..)
+import secrets # we need in order to get randomize picture name when we upload a new pic
+from PIL import Image # this class is installed with Pillow package (pip3 install Pillow) so we can autoresize big pictures to prevent the site from loading big files
+from flask import render_template, url_for, flash, redirect, request 
+from flaskblog import app, db, bcrypt
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm # we need UpdateAccountForm so we can use 
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user, login_required # imports that class so we can check the user email and password validation with the db data, and with current user we can deliver user a
+from flask_login import login_user, current_user, logout_user, login_required 
 
 
 posts = [
@@ -34,14 +37,13 @@ def about():
 
 @app.route("/register", methods =['GET', 'POST'])
 def register():
-    if current_user.is_authenticated: # current user check
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        #so if the form valid to submit (all length and signs are correct) we can hash the password and do the authentication procedure 
-        hashed_pasword = bcrypt.generate_password_hash(form.password.data).decode('utf-8') # "form.password.data" is the password that user has entered and ".decode('utf-8')" is needed to decde from bytes to a regular string
-        user = User(username = form.username.data, email = form.email.data, password = hashed_pasword) # getting the data from user into the db, where we get allready hashed password with "password = hashed_pasword"
-        #saving the entered dta to the db
+        
+        hashed_pasword = bcrypt.generate_password_hash(form.password.data).decode('utf-8') 
+        user = User(username = form.username.data, email = form.email.data, password = hashed_pasword)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created ! You are now able to log in', 'success')
@@ -51,17 +53,15 @@ def register():
 
 @app.route("/login", methods =['GET', 'POST'])
 def login():
-    if current_user.is_authenticated: # current user check
+    if current_user.is_authenticated: 
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        # before we were simply checking the hardcoded data, and now we need to check the database for valid data 
-        # firstly the user will be logged in with an email, so we need to write a check for the email in the db:
-        user = User.query.filter_by(email = form.email.data).first() # finds the first User by entered email in the id
-        if user and bcrypt.check_password_hash(user.password, form.password.data): # so if the user exist and the password the user entered is valid we want to login the user and we also need to import the login user function 
-            login_user(user, remember = form.remember.data) # "user" - what we want to login, "remember" - remember me form (boolean) we have created
-            next_page = request.args.get('next') # this line
-            return redirect(next_page) if next_page else redirect(url_for('home')) # use of ternary conditional 
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+            next_page = request.args.get('next') 
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash("login unsuccessful. Pls check ur email and password", "danger")
     return render_template('login.html', title = 'Login', form = form)
@@ -73,9 +73,44 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
-@login_required #extention knows that we need to be logged in to access this route 
+# logic for uploading a new picture as avatar 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8) # saves picture we upload with random hex token 8 bytes
+    _, f_ext = os.path.splitext(form_picture.filename) # we need the file extention at the end of the filename. we do not need to grab the file name so we can use underscore "_" to through away a variable name 
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn) #full path to the package directory
+    
+    # resizing picture (the upload image might be too big, so the page will load slowly in some cases), so we can change it to our needed size
+    output_size = (125, 125) # sets the size...
+    i = Image.open(form_picture) #
+    i.thumbnail(output_size)
+    i.save(picture_path) # saves resized pic to it's path
+
+    return picture_fn
+
+
+@app.route("/account", methods =['GET', 'POST']) 
+@login_required
 def account():
-    return render_template('account.html', title = 'Account')
+    # creates an instance of that Update form 
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data: 
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated !', 'success')
+        return redirect(url_for('account')) # we need to redirect right after data submitted, cuz of get\post method
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file) # it grabs the image in the user model (models.py) where we hardcoded that the default user image is the "default.jpg"
+    return render_template('account.html', title = 'Account',
+                            image_file=image_file, form=form)
+
+
+    
 
 
